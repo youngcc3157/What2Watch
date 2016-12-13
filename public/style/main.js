b97057e56c107487ab1cfa3fcf06c1cc524196c7
youngcc3157 <zzdudcjfzz@hotmail.com>
2016-12-13 11:54:05 -0500

    added initial iframe to display movie info

--- public/style/main.js
+++ public/style/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	$('#text').css('visibility','visible').hide().fadeIn(2000);
+	$("#container").load("https://www.rottentomatoes.com/m/the_secret_life_of_pets #center");
 });
+
+
+
+function myFunction() {
+    var x = document.createElement("IFRAME");
+    x.setAttribute("src", "http://www.w3schools.com");
+    document.body.appendChild(x);
+}
