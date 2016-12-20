b2bcb9e2f23cf6b642b112b763d60489b1871da7
youngcc3157 <zzdudcjfzz@hotmail.com>
2016-12-19 22:52:47 -0500

    enhanced jQuery animation by removing space

--- public/style/main.js
+++ public/style/main.js
@@ -3,10 +3,21 @@ $(document).ready(function() {
 });
 
 $('.button').click(function(){
-	$('#container, #question').css('visibility','visible').hide().fadeIn(2000);
+	$('#container, #question').css({"visibility": "visible"}).hide().fadeIn(2000);
 	$('html, body').animate({ 
-      scrollTop: $('#block').offset().top 
-  }, 800); 
+		scrollTop: $('#block').offset().top 
+	}, 800);
+	$('#container').css('height', '660px');
+	$('#iframe').css({
+		"position": "relative",
+		"top": "305px",
+		"border": "0px none", 
+		"margin-left": "-185px", 
+		"height": "1200px",
+		"margin-top": "-533px", 
+		"width": "1200px",
+		"right": "200px"
+	});
 });
 
 function myFunction() {
