5acfba42459ebfbd61dffcebc1c4bc6aa993fce9
youngcc3157 <zzdudcjfzz@hotmail.com>
2017-01-03 14:17:25 -0500

    database has been connected and returns random movies now

--- public/getMovie.php
+++ public/getMovie.php
@@ -5,22 +5,36 @@ $bar = isset($_POST['input']) ? $_POST['input'] : null;
 require_once('../connect.php');
 
 if ($bar === "Happy"){
-	echo "titanic";
+	$query = "SELECT link FROM movies WHERE genre = 'Action' OR genre = 'Drama' ORDER BY RAND() LIMIT 1";
 } 
 else if ($bar === "Sad") {
-	echo "titanic";
+	$query = "SELECT link FROM movies WHERE genre = 'Action' OR genre = 'Drama' ORDER BY RAND() LIMIT 1";
 }
 else if ($bar === "Calm") {
-	echo "titanic";
+	$query = "SELECT link FROM movies WHERE genre = 'Action' OR genre = 'Drama' ORDER BY RAND() LIMIT 1";
 }
 else if ($bar === "Excited") {
-	echo "thor";
+	$query = "SELECT link FROM movies WHERE genre = 'Action' OR genre = 'Horror' ORDER BY RAND() LIMIT 1";
 }
 else if ($bar === "Adventurous") {
-	echo "deadpool";
+	$query = "SELECT link FROM movies WHERE genre = 'Action' OR genre = 'Horror' ORDER BY RAND() LIMIT 1";
 }
 
+$response = @mysqli_query($conn, $query);
+if (!$response) {
+    die('Invalid query: ' . mysql_error());
+}
 
+if($response){
+	$row = mysqli_fetch_array($response);
+	echo $row['link'];
+} else {
+	echo "Couldn't issue database query";
+	
+	echo mysqli_error($conn);
+}
+
+mysqli_close($conn)
 
 
 ?>
